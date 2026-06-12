d75a032682148b6b8fd2209e54aba37e8bcee22e
Dieni Marques <dienimarques@hotmail.com>
2026-06-11 21:28:27 -0300

    Alterei um item da lista

--- data/produtos.ts
+++ data/produtos.ts
@@ -2,7 +2,7 @@ import { Produto } from "../classes/Produto";
 
     export const produtos: Produto[] = [
         new Produto(1, "Mouse", 50, 10),
-        new Produto(2, "Teclado", 120, 5),
+        new Produto(2, "CPU", 1200, 5),
         new Produto (3, "Monitor", 800, 2),
     ];
 
